c227ee587a10fb57de6d17f1eac2aeb69692fcff
Hussein7819 <husssein.ashour.2005@gmail.com>
2024-12-05 01:55:35 +0200

    Department creation. GUI EXCLUDED from the project

--- src/main/java/com/example/criminalrecordproject/Main.java
+++ src/main/java/com/example/criminalrecordproject/Main.java
@@ -24,8 +24,32 @@ public class Main //extends Application {
     */
     public static void main(String[] args) {
 
-     /*launch();
-      */
+     //launch();
+
+
+       /* department d1 = new department("D1", "Homicide", "20/11/2024");
+        department d2 = new department("D2", "Cybercrime", "21/11/2024");
+        department d3 = new department("D3", "Robbery", "22/11/2024");
+        department d4 = new department("D4", "Narcotics", "23/11/2024");
+        department d5 = new department("D5", "Financialcrime", "24/11/2024");
+        department d6 = new department("D6", "Forensics", "25/11/2024");
+        department d7 = new department("D7", "Domesticviolence", "26/11/2024");
+        department d8 = new department("D8", "Terrorism", "27/11/2024");
+
+        System.out.println("The available departments:");
+
+        System.out.println(d1.getDetails());
+        System.out.println(d2.getDetails());
+        System.out.println(d3.getDetails());
+        System.out.println(d4.getDetails());
+        System.out.println(d5.getDetails());
+        System.out.println(d6.getDetails());
+        System.out.println(d7.getDetails());
+        System.out.println(d8.getDetails());*/
+
+
+
+
     }
 
 
