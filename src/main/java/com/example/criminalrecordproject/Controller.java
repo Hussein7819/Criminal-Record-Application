f0efcad9e8d4be83726b17732ffde4ab5f3b07f0
Ali Mohamed <alihashem4g@gmail.com>
2024-12-05 18:58:32 +0200

    Design of user and admin interface and also adding Admin login in login function of user

--- src/main/java/com/example/criminalrecordproject/Controller.java
+++ src/main/java/com/example/criminalrecordproject/Controller.java
@@ -1,6 +1,7 @@
 package com.example.criminalrecordproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 
 public class Controller {
--- src/main/java/com/example/criminalrecordproject/Model/user.java
+++ src/main/java/com/example/criminalrecordproject/Model/user.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class user {
-    ArrayList<String>  Username=new ArrayList<>();
-    ArrayList<String> Password=new ArrayList<>();
+    protected ArrayList<String> Username;
+    protected ArrayList<String> Password;
+    public user(ArrayList<String> Username, ArrayList<String> Password) {
+        this.Username = Username;
+        this.Password = Password;
+    }
    protected void addacounts(String username,String password){
         System.out.println("add account");
         Scanner sc=new Scanner(System.in);
@@ -18,12 +22,17 @@ public class user {
         System.out.println("Created an account successfully!");
     }
     protected void login(String username,String password){
+        String Admin_username="admin";
+        String Admin_password="admin";
         System.out.println("login");
         Scanner sc=new Scanner(System.in);
         System.out.println("enter username:");
         username=sc.nextLine();
         System.out.println("enter password:");
         password=sc.nextLine();
+        if (username.equals(Admin_username) && password.equals(Admin_password)){
+            System.out.println("Login successfully!");
+        }
         for(int i=0;i<Username.size();i++){
             if(username.equals(Username.get(i))&&password.equals(Password.get(i))){
                 System.out.println("login successfully");
