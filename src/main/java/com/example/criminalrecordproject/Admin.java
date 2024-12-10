84e1c9359da2521a16fe6a584ea0591670084e71
Yara <yarameri13@gmail.com>
2024-12-10 21:47:17 +0200

    Admin ---> Adddepertment name handelling Done

--- src/main/java/com/example/criminalrecordproject/Admin.java
+++ src/main/java/com/example/criminalrecordproject/Admin.java
@@ -8,18 +8,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Admin extends user {
-    int Adminid=1;
-    String Adminname="Admin";
+    int Adminid = 1;
+    String Adminname = "Admin";
 
     public Admin(String Username, String Password) {
         super(Username, Password);
     }
+
     public void Show_Admin_Menu(ArrayList<Department> departments) {
         Scanner input = new Scanner(System.in);
-        while (true)
-        {
-            try
-            {
+        while (true) {
+            try {
                 System.out.println("======================================================================================");
                 System.out.println("\nChoose an action:");
                 System.out.println("1. Add a new department");
@@ -30,10 +29,9 @@ public class Admin extends user {
                 int choice = input.nextInt();
                 input.nextLine();
 
-                switch (choice)
-                {
+                switch (choice) {
                     case 1:
-                        addDepartment(input,departments);
+                        addDepartment(input, departments);
                         break;
 
                     case 2:
@@ -52,35 +50,44 @@ public class Admin extends user {
                     default:
                         System.out.println("Invalid choice! Please try again.");
                 }
-            }
-            catch (InputMismatchException e)
-            {
+            } catch (InputMismatchException e) {
                 System.out.println("Invalid input! Please enter a valid number.");
                 input.nextLine();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("An unexpected error occurred: " + e.getMessage());
             }
         }
     }
+
     private static void addDepartment(Scanner input, ArrayList<Department> departments)
     {
         System.out.println("Enter Department ID:");
         String deptID = input.nextLine();
-
-
-        for (Department dept : departments)
-        {
-            if (dept.getDepartmentID().equals(deptID))
-            {
-                System.out.println("A department with this ID already exists! Please try again.");
-                return;
-            }
-        }
-
         System.out.println("Enter Department Name:");
-        String deptName = input.nextLine();
+        boolean x = true;
+        String deptName = "";
+        while (x)
+        {
+            boolean exist =false;
+            deptName = input.nextLine();
+            for (Department dept : departments) {
+                if (dept.getName().equals(deptName)) {
+                    System.out.println("A department with this name already exists! Please try again.");
+                    exist = true;
+                    break;
+
+                }
+            }
+
+            if (!exist)
+            {
+                System.out.println("A Department is Added successfully.");
+                x= false;
+
+
+            }
+
+        }
         System.out.println("Enter Date of Activation:");
         String activationDate = input.nextLine();
 
@@ -88,7 +95,7 @@ public class Admin extends user {
         System.out.println("Department added successfully!");
     }
 
-    private static void addOfficers(){
+    private static void addOfficers() {
         System.out.println("Which department do you want to add officers to? \n enter department ID");
     }
 
